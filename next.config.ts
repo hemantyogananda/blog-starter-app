30ae98e68d8bb7efa4243f8afd15883a038079ee
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-14 10:46:46 +0530

    Added basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+    basePath: isProd ? '/blog-starter-app' : "",
     output: 'export',
    
     // Optional: Change the output directory `out` -> `dist`
