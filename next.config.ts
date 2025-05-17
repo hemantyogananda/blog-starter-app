9d5fb5fdb4c124bf3ecd3447ebd2a664b5eaa7dc
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-17 07:41:39 +0530

    Reverted nextconfig

--- next.config.ts
+++ next.config.ts
@@ -6,21 +6,11 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
     basePath: isProd ? '/blog-starter-app' : "",
-    // output: 'export',
+    output: 'export',
    
     // Optional: Change the output directory `out` -> `dist`
     distDir: 'dist',
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'hemantyogananda.github.io',
-          port: '',
-          pathname: '/blog-starter-app/**',
-          search: '',
-        },
-      ],
-    },
+    NEXT_PUBLIC_BASE_PATH : "https://hemantyogananda.github.io/blog-starter-app/",
   }
    
   module.exports = nextConfig
