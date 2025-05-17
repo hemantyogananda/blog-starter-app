4bd076f60c4f0c0d0581f185e85feebe4d18d14b
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-17 07:31:03 +0530

    added images in nextconfig

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,21 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
     basePath: isProd ? '/blog-starter-app' : "",
-    output: 'export',
+    // output: 'export',
    
     // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
-    NEXT_PUBLIC_BASE_PATH : "https://hemantyogananda.github.io/blog-starter-app/",
+    // distDir: 'dist',
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'hemantyogananda.github.io',
+          port: '',
+          pathname: '/blog-starter-app/**',
+          search: '',
+        },
+      ],
+    },
   }
    
   module.exports = nextConfig
