8c4b680180edaec3116aa7428ae29fe01480833f
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-17 08:20:13 +0530

    Reverted

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import Avatar from "./avatar";
 import CoverImage from "./cover-image";
 import DateFormatter from "./date-formatter";
-import { basePath } from '../../../next.config';
 
 type Props = {
   title: string;
@@ -25,7 +24,7 @@ export function PostPreview({
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={`${basePath}/${coverImage}`} />
+        <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`} className="hover:underline">
