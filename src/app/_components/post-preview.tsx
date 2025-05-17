f1fd21ed4b751319d745d40dee96012622f969b6
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-17 08:12:59 +0530

    Added basepath for images

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Avatar from "./avatar";
 import CoverImage from "./cover-image";
 import DateFormatter from "./date-formatter";
+import basePath from '../../../next.config.ts';
 
 type Props = {
   title: string;
@@ -24,7 +25,7 @@ export function PostPreview({
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
+        <CoverImage slug={slug} title={title} src={`${basePath}/coverImage`} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`} className="hover:underline">
