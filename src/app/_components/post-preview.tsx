060ab90dc6496a25ebb8b14a15a1323997f39139
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-17 08:17:09 +0530

    Updated literals

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -25,7 +25,7 @@ export function PostPreview({
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={`${basePath}/coverImage`} />
+        <CoverImage slug={slug} title={title} src={`${basePath}/${coverImage}`} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`} className="hover:underline">
